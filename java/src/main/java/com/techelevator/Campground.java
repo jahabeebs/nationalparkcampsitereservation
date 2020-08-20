baf928c8434b53624c06ed2f0dbd435e45e1059a
Jacob Habib <habib.50@osu.edu>
2020-08-20 11:13:54 -0400

    Syncing with brad

--- java/src/main/java/com/techelevator/Campground.java
+++ java/src/main/java/com/techelevator/Campground.java
@@ -3,9 +3,40 @@ package com.techelevator;
 import java.math.BigDecimal;
 
 public class Campground {
-	private Long campground_id;
+	private long campground_id;
 	private String name;
 	private double openFromMM;
 	private double openToMM;
-	private BigDecimal money;
+	private BigDecimal dailyFee;
+	
+	public Long getCampground_id() {
+		return campground_id;
+	}
+	public void setCampground_id(Long campground_id) {
+		this.campground_id = campground_id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getOpenFromMM() {
+		return openFromMM;
+	}
+	public void setOpenFromMM(double openFromMM) {
+		this.openFromMM = openFromMM;
+	}
+	public double getOpenToMM() {
+		return openToMM;
+	}
+	public void setOpenToMM(double openToMM) {
+		this.openToMM = openToMM;
+	}
+	public BigDecimal getDailyFee() {
+		return dailyFee;
+	}
+	public void setDailyFee(BigDecimal dailyFee) {
+		this.dailyFee = dailyFee;
+	}
 }
--- java/src/main/java/com/techelevator/Park.java
+++ java/src/main/java/com/techelevator/Park.java
@@ -1,12 +1,56 @@
 package com.techelevator;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public class Park {
-	private Long parkId;
+	private long parkId;
 	private String name;
+	private String location;
 	private LocalDate establishedDate;
-	private double openToMM;
-	private BigDecimal money;
+	private int area; 
+	private long annualVisitorCount;
+	private String description;
+	
+	public long getParkId() {
+		return parkId;
+	}
+	public void setParkId(long parkId) {
+		this.parkId = parkId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public LocalDate getEstablishedDate() {
+		return establishedDate;
+	}
+	public void setEstablishedDate(LocalDate establishedDate) {
+		this.establishedDate = establishedDate;
+	}
+	public int getArea() {
+		return area;
+	}
+	public void setArea(int area) {
+		this.area = area;
+	}
+	public long getAnnualVisitorCount() {
+		return annualVisitorCount;
+	}
+	public void setAnnualVisitorCount(long annualVisitorCount) {
+		this.annualVisitorCount = annualVisitorCount;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
